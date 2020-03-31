23a89c1cc3693c2c8c972a83949026d203b37114
jgmv <jg.macia@gmail.com>
2020-03-31 15:08:22 +0200

    2020-03-31

---
+++ cd-hit_clustering.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# cluster sequences with cd-hit, and formats output
+cd-hit_clustering () {
+  if [ -x "$(command -v cdhit)" ]
+  then
+    printf "cdhit found\\n"
+  else
+    printf "cdhit not found. Please install.\\n"
+    return   
+  fi
+  
+  # run cd-hit
+  cdhit -i $2 -o .temp -c $1 -g 1
+
+  # representative sequences
+  mv .temp $3.fasta
+
+  # generate clusters file
+  sed -i 's/.*>//' .temp.clstr
+  sed -i 's/[...].*//' .temp.clstr
+  sed -i 's/Cluster [0-9]*//' .temp.clstr
+  sed -i ':a;N;$!ba;s/\n\n/;/g' .temp.clstr
+  sed -i ':a;N;$!ba;s/\n/ /g' .temp.clstr
+  sed -i ':a;N;$!ba;s/;/\n/g' .temp.clstr
+  sed -i 's/;/\n/g' .temp.clstr
+  sed -i 's/^ *//;s/ *$//;s/  */ /;' .temp.clstr
+
+  # generate clusters tab
+  otuList .temp.clstr $3.csv
+
+  # delete temporaty file
+  rm .temp.clstr
+}
--- otuList.sh
+++ otuList.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 
 # generate list of OTUs from BlastClust output
-otuList () {
-  cp $1 clusters
-  sed -i 's/.$//' clusters # remove last blank space from each line
-  sed -i 's/^/ /' clusters # add extra blank space in each line
+otuList() {
+ cp $1 .temp
+  sed -i 's/^ *//;s/ *$//;s/  */ /;' .temp # remove trailing spaces
+  sed -i 's/^/ /' .temp # add extra blank space in each line
   >$2
-  for i in $( seq 1 $( wc -l clusters | cut -d' ' -f1 ) )
+  NCLUST=$( wc -l .temp | cut -d' ' -f1 )
+  for i in $( seq 1 $NCLUST )
   do
     if [ $i -lt 10 ]
-    then sed -e $i'!d' -e "s/ / OTU00$i\t/g" clusters >>$2
+     then sed -e $i'!d' -e "s/ / OTU0000$i\t/g" .temp >>$2
     elif [ $i -lt 100 ]
-    then sed -e $i'!d' -e "s/ / OTU0$i\t/g" clusters >>$2
-    else sed -e $i'!d' -e "s/ / OTU$i\t/g" clusters >>$2
+     then sed -e $i'!d' -e "s/ / OTU000$i\t/g" .temp >>$2
+    elif [ $i -lt 1000 ]
+     then sed -e $i'!d' -e "s/ / OTU00$i\t/g" .temp >>$2
+    elif [ $i -lt 10000 ]
+     then sed -e $i'!d' -e "s/ / OTU0$i\t/g" .temp >>$2
+    else sed -e $i'!d' -e "s/ / OTU$i\t/g" .temp >>$2
     fi
   done
   sed -i 's/^ //' $2 # remove first blank space frome each line
   sed -i ':a;N;$!ba;s/ /\n/g' $2 # replace every blank space by a new line
-  rm clusters
-}
+  
+  # remove extra zeros from OTU names
+  if [ $NCLUST -lt 10 ]
+    then sed -i 's/^OTU0000/OTU/g' $2
+  elif [ $NCLUST -lt 100 ]
+    then sed -i 's/^OTU000/OTU/g' $2
+  elif [ $NCLUST -lt 1000 ]
+    then sed -i 's/^OTU00/OTU/g' $2
+  elif [ $NCLUST -lt 10000 ]
+    then  sed -i 's/^OTU0/OTU/g' $2
+  fi
 
+  # remove temporary file
+  rm .temp
+}
