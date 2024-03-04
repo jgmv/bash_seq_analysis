609aba0a08e1c064458b3bbee0df1d21c3680117
jgmv <53367553+jgmv@users.noreply.github.com>
2024-03-04 20:56:32 +0100

    Update removeTaxonTag.sh

--- removeTaxonTag.sh
+++ removeTaxonTag.sh
@@ -3,14 +3,7 @@
 # remove taxon tags from UNITE taxonomy files and modify output
 removeTaxonTag () {
   cp $1 $2
-  sed -i 's/k__//g' $2
-  sed -i 's/p__//g' $2
-  sed -i 's/c__//g' $2
-  sed -i 's/o__//g' $2
-  sed -i 's/f__//g' $2
-  sed -i 's/g__//g' $2
-  sed -i 's/s__//g' $2
-  sed -i 's/\t/;/g' $2
+  sed -i 's/[a-z]__//g' $2
   sed -i 's/;$//' $2
   sed -i "1s/^/group;kingdom;phylum;class;order;family;genus;species\n/" $2
   sed -i 's/Otu/OTU/g' $2
